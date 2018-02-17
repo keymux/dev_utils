84a2d3bdca04fa736fdcc6b9e71aee51249f5e0c
Henry <hankbrown@gmail.com>
2018-02-16 19:35:49 -0800

    Abstract directory-changed logic into a library utility.

--- .changes/pending.md
+++ .changes/pending.md
@@ -2,3 +2,4 @@
 
 Feature:
   * DEV-1: Added bitbucket comment generation for changelogs
+  * DEV-5: Added github comment generation for changelogs
--- bin/directory-changed.js
+++ bin/directory-changed.js
@@ -1,69 +1,29 @@
 #!/usr/bin/env node
 
-const { diffCurrentHeadWithReference, getChanges } = require("../lib/git");
-
-const bitbucket = require("../lib/formatting/diff/bitbucket");
-const github = require("../lib/formatting/diff/github");
-
-const { print, unhandledPromiseRejectionHandler } = require("../lib/utils");
-
-process.on("unhandledRejection", unhandledPromiseRejectionHandler);
+const { print } = require("../lib/utils");
 
 const parseArgs = require("minimist");
 
 const cliArgs = parseArgs(process.argv.slice(2));
 
-const DIFF_FORMAT_ENGINES = [
-  {
-    bitbucket: bitbucket.formatPatchesForComment,
-  },
-  {
-    github: github.formatPatchesForComment,
-  },
-];
-
 const main = () => {
-  cliArgs.startsWith = cliArgs.startsWith || ".changes";
-  cliArgs.noChangeMessage =
-    cliArgs.noChangeMessage || "No changelog modifications were found";
-  cliArgs.onErrorExitCode = cliArgs.onErrorExitCode || -1;
-  cliArgs.gitDir = cliArgs.gitDir || "./.git";
-  cliArgs.diffAgainstReference =
-    cliArgs.diffAgainstReference || "refs/remotes/origin/master";
-  cliArgs.diffFormatEngine = cliArgs.diffFormatEngine || "github";
+  const options = Object.assign({}, cliArgs);
 
-  const patchesFilter = patch => {
-    return patch
-      .newFile()
-      .path()
-      .startsWith(cliArgs.startsWith);
-  };
-
-  const diffOpts = { contextLines: 0 };
-
-  return diffCurrentHeadWithReference(
-    cliArgs.gitDir,
-    cliArgs.diffAgainstReference,
-    {
-      diffOpts,
-    }
-  )
-    .then(diff => getChanges(diff, { patchesFilter }))
-    .then(data => {
-      if (!data.patches || data.patches.length === 0) {
-        console.log(cliArgs.noChangeMessage);
-
-        throw new Error(cliArgs.noChangeMessage);
-      }
-
-      console.log(formatPatches(data.patches, { bitbucketComment: true }));
-    });
+  return require("../index.js").simple.diff(options);
 };
 
-main()
-  .then()
-  .catch(err => {
-    print(err);
+if (!module.parent) {
+  main()
+    // Exit with the code provided
+    .then(process.exit)
+    .catch(err => {
+      print("Unhandled error:");
+      print(err);
 
-    process.exit(cliArgs.onErrorExitCode);
-  });
+      process.exit(cliArgs.onErrorExitCode || -1);
+    });
+} else {
+  module.exports = {
+    main,
+  };
+}
