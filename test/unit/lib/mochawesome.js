29bf75e3435eefb430a7ea855fae3937aafd8ec6
Henry <hankbrown@gmail.com>
2018-02-22 23:33:35 -0800

    mochawesome test file

---
+++ test/unit/lib/mochawesome.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const { expect } = require("chai");
+
+const root = process.env.ROOT_DIR;
+const testDir = "test";
+const libDir = "lib";
+
+const util = require(path.join(root, testDir, libDir, "util"));
+
+const mochawesome = require(path.join(root, libDir, "mochawesome"));
+
+describe("mochawesome", () => {
+  describe("exports", () => {
+    const expectedExports = ["statsToMarkdown"];
+
+    util.expectExports(mochawesome, expectedExports);
+  });
+});
