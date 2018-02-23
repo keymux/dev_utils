507e6627d244e44354dd64b8da482b8e5725425d
Henry <hankbrown@gmail.com>
2018-02-23 00:27:31 -0800

    Lingering parsing code from the old formatter caused Errors to get cut off for some reason.

--- scripts/test:lint.sh
+++ scripts/test:lint.sh
@@ -6,10 +6,9 @@ SCRIPTS_DIR="${SCRIPTS_DIR:?}"
 ROOT_DIR=$(realpath "${SCRIPTS_DIR}/..")
 REPORTS_DIR="${ROOT_DIR}/reports"
 GITHUB_REPORT_FILE="${REPORTS_DIR}/lint.githubCommentFile"
-GITHUB_REPORT_FILE_TMP="${GITHUB_REPORT_FILE}.tmp"
 FORMATTER="${ROOT_DIR}/bin/eslint-markdown-formatter.js"
 
-echo -ne "## Linter\n\n" | tee -a "${GITHUB_REPORT_FILE_TMP}"
+echo -ne "## Linter\n\n" | tee -a "${GITHUB_REPORT_FILE}"
 
 if [ ! -z "${JOB_BASE_NAME}" ]; then
   # Act as if on Jenkins if the JOB_BASE_NAME environment variable exists
@@ -19,15 +18,9 @@ fi
 yarn -s eslint \
   --ignore-path .gitignore \
   ${FORMAT_ARG} \
-  "${ROOT_DIR}/**/*.js" 2>&1 | tee -a "${GITHUB_REPORT_FILE_TMP}"
+  "${ROOT_DIR}/**/*.js" 2>&1 | tee -a "${GITHUB_REPORT_FILE}"
 
 ESLINT_RESULT=${PIPESTATUS[0]}
-
-DETAILS_LINE_NO=$(grep -nE '^## Details' ${GITHUB_REPORT_FILE_TMP} | awk -F':' '{print $1}')
-
-let USEFUL_LINE_COUNT=${DETAILS_LINE_NO}-1
-
-head -n ${USEFUL_LINE_COUNT} ${GITHUB_REPORT_FILE_TMP} > ${GITHUB_REPORT_FILE}
 
 echo -ne "\n\n" | tee -a "${GITHUB_REPORT_FILE}"
 
