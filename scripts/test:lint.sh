18499740418645f2042386c3db555fc921d0d1c6
Henry <hankbrown@gmail.com>
2018-02-22 22:17:36 -0800

    Reduce output for linter

--- scripts/test:lint.sh
+++ scripts/test:lint.sh
@@ -6,6 +6,7 @@ SCRIPTS_DIR="${SCRIPTS_DIR:?}"
 ROOT_DIR=$(realpath "${SCRIPTS_DIR}/..")
 REPORTS_DIR="${ROOT_DIR}/reports"
 GITHUB_REPORT_FILE="${REPORTS_DIR}/lint.githubCommentFile"
+GITHUB_REPORT_FILE_TMP="${GITHUB_REPORT_FILE}.tmp"
 FORMATTER="${ROOT_DIR}/node_modules/eslint-formatter-markdown/markdown.js"
 
 if [ -z "${ghprbTargetBranch}" ]; then
@@ -18,14 +19,20 @@ if [ -z "${ghprbTargetBranch}" ]; then
   fi
 fi
 
-echo -ne "## Linter\n\n" | tee -a "${GITHUB_REPORT_FILE}"
+echo -ne "## Linter\n\n" | tee -a "${GITHUB_REPORT_FILE_TMP}"
 
 yarn -s eslint \
   --ignore-path .gitignore \
   --format "${FORMATTER}" \
-  "${ROOT_DIR}/**/*.js" 2>&1 | tee -a "${GITHUB_REPORT_FILE}"
+  "${ROOT_DIR}/**/*.js" 2>&1 | tee -a "${GITHUB_REPORT_FILE_TMP}"
 
 ESLINT_RESULT=${PIPESTATUS[0]}
+
+DETAILS_LINE_NO=$(grep -nE '^## Details' ${GITHUB_REPORT_FILE_TMP} | awk -F':' '{print $1}')
+
+let USEFUL_LINE_COUNT=${DETAILS_LINE_NO}-1
+
+head -n ${USEFUL_LINE_COUNT} ${GITHUB_REPORT_FILE_TMP} > ${GITHUB_REPORT_FILE}
 
 echo -ne "\n\n" | tee -a "${GITHUB_REPORT_FILE}"
 
